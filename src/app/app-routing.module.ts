61ed09cc098dfe5a6cbc756badacbbde08d10409
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-09-14 10:10:06 -0300

    feat: configuring routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateStickerComponent } from './components/stickers/create-sticker/create-sticker.component';
+import { ListStickersComponent } from './components/stickers/list-stickers/list-stickers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'list-stickers'
+  },
+  {
+    path: 'create-sticker',
+    component: CreateStickerComponent
+  },
+  {
+    path: 'list-stickers',
+    component: ListStickersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
